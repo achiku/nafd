6a8ffa13511659a5a63826aef644926d1161f189
Akira Chiku <akira.chiku@gmail.com>
2019-03-30 13:07:14 +0900

    Update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { SheetService } from './sheet.service';
 
 declare var global: any;
 
-global.createNewFile = (): void => {
+global.createNewFile = (): string => {
+  Logger.log('test');
   const ss = SheetService.createInitialFile('New file');
   ss.getRange('A2').setValue('Happy gas!');
+  return 'hello world';
 };
